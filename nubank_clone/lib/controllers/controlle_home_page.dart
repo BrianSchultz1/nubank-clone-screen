5eff846a4940dacedd4964093b2dea2b9b9b5c34
Brian Schultz <68474136+BrianSchultz1@users.noreply.github.com>
2023-05-14 11:41:00 -0300

    Update controlle_home_page.dart

--- nubank_clone/lib/controllers/controlle_home_page.dart
+++ nubank_clone/lib/controllers/controlle_home_page.dart
@@ -3,7 +3,7 @@ import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 class ControllerHomePage extends GetxController {
   String saldo = 'R\$ 873.244,00';
   bool eyesValue = true;
-
+ 
   void showValue() {
     eyesValue = !eyesValue;
 
